531c6850c4fd26e88f6983e7f4b1f30099afff96
Onwuka ekene <onwukadavid732@gmail.com>
2024-11-13 03:26:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+document.querySelector("img.tree").addEventListener("click", tree);
+
+function tree() {
+    alert("A picture of the most beautiful tree in the forest");
+}
+
+document.querySelector("img.mount").addEventListener("click", mount);
+
+function mount() {
+    alert("An amazing mountain picture taken by one of our best");
+}
+
+document.querySelector("img.water").addEventListener("click", water);
+
+function water() {
+    alert("A breath taking view (Some say someone got jealous of the montain picture and took this😂)"); 
+}
+
+document.querySelector("img.grass").addEventListener("click", grass);
+
+function grass() {
+    alert("A garden");
+}
+
+document.querySelector("img.aston").addEventListener("click", aston);
+
+function aston() {
+    alert("An aston-martin");
+}
+
+document.querySelector("img.benz").addEventListener("click", benz);
+
+function benz() {
+    alert("BENZ");
+}
+
+document.querySelector("img.ferrari").addEventListener("click", ferrari);
+
+function ferrari() {
+    alert("ferrari");
+}
+
+document.querySelector("img.mclaren").addEventListener("click", mclaren);
+
+function mclaren() {
+    alert("Mclaren");
+}
+
+document.querySelector("img.porsche").addEventListener("click", porsche);
+
+function porsche() {
+    alert("Porsche");
+}
